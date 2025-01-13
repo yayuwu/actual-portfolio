2ea5b9b861ddb1fdf84a009a128ce1905a934e98
Yael Correa <yscorrea10@gmail.com>
2025-01-13 18:46:20 -0300

    Responsive details

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <p className='text-base md:text-xl ms-3'>Home</p>
           </div>
         <h1 className='text-4xl xl:text-6xl 2xl:text-6xl'>I&apos;m Yael <span className='block text-colorGreen'>Front End Developer</span> </h1>
-        <p className='text-base xl:text-xl lg:w-[400px] xl:w-[700px] my-4 mb-5 2xl:mb-8 leading-7 xl:leading-9 font-semibold'>I&apos;m a specialist in creating attractive and functional interfaces, maintaining your brand identity and optimizing the performance of your website or application to the fullest.</p>
+        <p className='text-base xl:text-xl lg:w-[400px] xl:w-[700px] my-4 mt-8 xl:mt-4 mb-5 2xl:mb-8 leading-7 xl:leading-9 font-semibold'>I&apos;m a specialist in creating attractive and functional interfaces, maintaining your brand identity and optimizing the performance of your website or application to the fullest.</p>
         <GreenBtn href='https://drive.google.com/file/d/10e2K19HFqRnW2gFroZIlcEBr_aWTpCJG/view?usp=drive_link'>Download CV</GreenBtn>
       </section>
       <section className='hidden lg:block 3xl:me-36'>
