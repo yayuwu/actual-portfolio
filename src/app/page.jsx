97c3003772c535d0ca7e724fec83429822a13621
Yael Correa <yscorrea10@gmail.com>
2025-01-13 19:07:33 -0300

    Details

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className='font-semibold mt-12 md:mt-0  mx-7 md:mx-14 2xl:mx-40 flex justify-between items-center flex-wrap h-[60vh]'>
+    <div className='font-semibold mt-12 md:mt-0  mx-7 md:mx-14 2xl:mx-40 flex justify-between items-center flex-wrap'>
       <section>
           <div className='flex items-center'>
             <Image width='25' height='2' src="/svg/Line.svg" alt="Logo" />
             <p className='text-base md:text-xl ms-3'>Home</p>
           </div>
-        <h1 className='text-4xl xl:text-6xl 2xl:text-6xl'>I&apos;m Yael <span className='block text-colorGreen'>Front End Developer</span> </h1>
+        <h1 className='text-3xl xl:text-6xl 2xl:text-6xl'>I&apos;m Yael <span className='block text-colorGreen'>Front End Developer</span> </h1>
         <p className='text-base xl:text-xl lg:w-[400px] xl:w-[700px] my-4 mt-8 xl:mt-4 mb-5 2xl:mb-8 leading-7 xl:leading-9 font-semibold'>I&apos;m a specialist in creating attractive and functional interfaces, maintaining your brand identity and optimizing the performance of your website or application to the fullest.</p>
         <GreenBtn href='https://drive.google.com/file/d/10e2K19HFqRnW2gFroZIlcEBr_aWTpCJG/view?usp=drive_link'>Download CV</GreenBtn>
       </section>
