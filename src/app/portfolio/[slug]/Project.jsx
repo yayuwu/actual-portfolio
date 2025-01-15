175874440fc5284bd1cb2d42dee8dc62d141bee7
Yael Correa <yscorrea10@gmail.com>
2025-01-15 16:21:58 -0300

    Modal

---
+++ src/app/portfolio/[slug]/Project.jsx
@@ -0,0 +1,90 @@
+"use client"
+import React, { useState } from 'react'
+import Image from 'next/image'
+import svgLine from '../../../../public/svg/Line.svg'
+import svgWebsite from '../../../../public/svg/website.svg'
+import svgGithub from '../../../../public/svg/github.svg'
+import { TechBtn } from '@/app/ui/buttons'
+import { ArrowCircleLeftOutlined, DesignServices } from '@mui/icons-material'
+import { Masonry } from '@mui/lab'
+
+const Project = ({ projectData }) => {
+    const [clickImage, setClickImage] = useState(null)
+    const { name, description, github, design, web, tech, images } = projectData
+
+    const handleClickImage = (img) => {
+        setClickImage(img)
+    }
+
+    const handleCloseImage = () => {
+        setClickImage(null)
+    }
+
+  return (
+    <div className='mt-12 md:mt-0  mx-7 md:mx-14 2xl:mx-40'>
+      {/* Modal */}
+      {clickImage && (
+        <section className='fixed inset-0 z-50 flex items-center justify-center'>
+            <div className="relative">
+            <img
+                className="rounded-xl md:rounded-3xl max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl 2xl:max-w-3xl 3xl:max-w-4xl"
+                src={clickImage}
+                alt=""
+            />
+            <button
+                className="absolute top-5 right-5"
+                aria-label="Cerrar"
+                onClick={handleCloseImage}
+            >
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="34"
+                viewBox="0 96 960 960"
+                width="34"
+                fill="black"
+                >
+                <path d="M256 860 196 800l224-224-224-224 60-60 224 224 224-224 60 60-224 224 224 224-60 60-224-224-224 224Z" />
+                </svg>
+            </button>
+            </div>
+        </section>
+      )}
+          <div className='flex items-center'>
+            <Image width='25' height='2' src={svgLine} alt="" />
+            <p className='text-base md:text-xl ms-3 font-semibold'>Works & Projects</p>
+          </div>
+          <h1 className='text-4xl xl:text-6xl 2xl:text-6xl font-semibold'>{name}</h1>
+            <div className='flex gap-4 items-center'>
+                { github ? (<a href={github} target='_blank' className='cursor-pointer'><Image width={40} height={40} src={svgGithub} alt='github'/></a>) : null }
+                { web ? (<a href={web} target='_blank' className='cursor-pointer'><Image width={48} height={48} src={svgWebsite} alt='website'/></a>) : null }
+                { design ? (<a href={design} target='_blank' className='cursor-pointer'><DesignServices sx={{color: '#00BFAE'}}/></a>) : null}
+            </div>
+            <p className='text-md font-medium mt-5 md:w-[600px]'>{description}</p>
+          <section className='flex flex-wrap gap-4 my-4'>
+            {tech.map((t, index) => (
+                <TechBtn key={index} textColor="text-white" backgroundColor="bg-colorGreen">{t}</TechBtn>
+            ))}
+          </section>
+          <section className='mt-10 flex justify-center '>
+                <Masonry columns={{
+                xs: 2,
+                lg: 3,
+                }} 
+                spacing={{
+                xs: 2,
+                md: 5
+                }}>
+                {images.map((img, index) => (
+                    <button key={index} onClick={() => handleClickImage(img)}><img src={img} alt={`${name}-${index}`} className='rounded-xl md:rounded-3xl hover:bg-gray-800 z-10'/></button>
+                ))}
+                </Masonry>
+            </section>
+           <section className='flex justify-center mb-5'>
+                <a href={`${process.env.NEXT_PUBLIC_API_URL}/portfolio`} className='flex items-center px-4 pe-7 py-2 text-sm md:text-lg rounded-full font-medium w-fit bg-colorGreen'><ArrowCircleLeftOutlined sx={{ width: '40px'}} />Go back</a>
+           </section>
+    </div>
+  )
+}
+
+
+export default Project
--- src/app/portfolio/[slug]/page.jsx
+++ src/app/portfolio/[slug]/page.jsx
@@ -1,73 +1,15 @@
-import React from 'react'
-import Image from 'next/image'
-import svgLine from '../../../../public/svg/Line.svg'
-import svgWebsite from '../../../../public/svg/website.svg'
-import svgGithub from '../../../../public/svg/github.svg'
-import { TechBtn } from '@/app/ui/buttons'
-import { ArrowCircleLeftOutlined, DesignServices } from '@mui/icons-material'
-import { Masonry } from '@mui/lab'
+import Project from './Project';
 
 async function fetchProjectData(slug) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?slug=${slug}`); // Asegúrate de que la URL sea correcta para tu API
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?slug=${slug}`)
   if (!res.ok) {
-    console.error('Error al obtener los datos del proyecto');
-    return null;
+    throw new Error('Error al obtener los datos del proyecto')
   }
-  return await res.json();
+  return await res.json()
 }
 
-const Project = async ({ params }) => {
-  const { slug } = params
-  const projectData = await fetchProjectData(slug)
+export default async function Page({ params }) {
+  const projectData = await fetchProjectData(params.slug);
 
-  // if(!projectData ){
-  //   return (
-  //     <div className='mt-12 md:mt-0  mx-7 md:mx-14 2xl:mx-40'>
-  //       <h1 className='text-4xl xl:text-6xl 2xl:text-6xl font-semibold'>Proyecto no encontrado</h1>
-  //     </div>
-  //   )
-  // }
-
-  const { name, description, github, design, web, tech, images } = projectData
-
-  return (
-    <div className='mt-12 md:mt-0  mx-7 md:mx-14 2xl:mx-40'>
-          <div className='flex items-center'>
-            <Image width='25' height='2' src={svgLine} alt="" />
-            <p className='text-base md:text-xl ms-3 font-semibold'>Works & Projects</p>
-          </div>
-          <h1 className='text-4xl xl:text-6xl 2xl:text-6xl font-semibold'>{name}</h1>
-            <div className='flex gap-4 items-center'>
-                { github ? (<a href={github} target='_blank' className='cursor-pointer'><Image width={40} height={40} src={svgGithub} alt='github'/></a>) : null }
-                { web ? (<a href={web} target='_blank' className='cursor-pointer'><Image width={48} height={48} src={svgWebsite} alt='website'/></a>) : null }
-                { design ? (<a href={design} target='_blank' className='cursor-pointer'><DesignServices sx={{color: '#00BFAE'}}/></a>) : null}
-            </div>
-            <p className='text-md font-medium mt-5 md:w-[600px]'>{description}</p>
-          <section className='flex flex-wrap gap-4 my-4'>
-            {tech.map((t, index) => (
-                <TechBtn key={index} textColor="text-white" backgroundColor="bg-colorGreen">{t}</TechBtn>
-            ))}
-          </section>
-          <section className='mt-10 flex justify-center '>
-                <Masonry columns={{
-                xs: 2,
-                lg: 3,
-                }} 
-                spacing={{
-                xs: 2,
-                md: 5
-                }}>
-                {images.map((img, index) => (
-                    <img key={index} src={img} alt={`${name}-${index}`} className='rounded-xl md:rounded-3xl hover:bg-gray-800 z-10'/>
-                ))}
-                </Masonry>
-            </section>
-           <section className='flex justify-center mb-5'>
-                <a href={`${process.env.NEXT_PUBLIC_API_URL}/portfolio`} className='flex items-center px-4 pe-7 py-2 text-sm md:text-lg rounded-full font-medium w-fit bg-colorGreen'><ArrowCircleLeftOutlined sx={{ width: '40px'}} />Go back</a>
-           </section>
-    </div>
-  )
+  return <Project projectData={projectData} />
 }
-
-
-export default Project
